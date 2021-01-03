97415e129f53c27804035815e4c27b65e225d2eb
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2021-01-03 20:16:51 +0300

    added composition api

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,27 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        compositionnuxt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <main class="container">
+    <ul>
+      <li @click="alertUnit" v-for="(item, index) in theList" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </main>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { defineComponent, ref } from '@vue/composition-api'
 
-export default Vue.extend({})
+export default defineComponent({
+  setup() {
+    const theList = ['test1', 'test2', 'test3']
+
+    const alertUnit = (e: any) => {
+      alert(e.target.textContent)
+    }
+
+    return { theList, alertUnit }
+  },
+})
 </script>
 
 <style>
@@ -42,34 +33,7 @@ export default Vue.extend({})
   align-items: center;
   text-align: center;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+li {
+  font-size: 50px;
 }
 </style>
